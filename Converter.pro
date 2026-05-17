acc1ed96dd982c0a65a79dc50ea58094a053f97a
Егор <enor2000@mail.ru>
2026-05-17 22:17:48 +0300

    basic options BigInteger

--- Converter.pro
+++ Converter.pro
@@ -7,11 +7,13 @@ CONFIG += c++17
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
+    biginteger.cpp \
     converter.cpp \
     main.cpp \
     mainwindow.cpp
 
 HEADERS += \
+    biginteger.h \
     converter.h \
     mainwindow.h
 
---
+++ biginteger.h
@@ -0,0 +1,35 @@
+#pragma once
+#ifndef BIGINTEGER_H
+#define BIGINTEGER_H
+#include <string>
+#include <vector>
+
+class BigInteger
+{
+public:
+    BigInteger();
+    BigInteger(unsigned long long value);
+    BigInteger(const std::string& value);
+
+    std::string toString() const;
+    bool isZero() const;
+
+    friend bool operator<(const BigInteger& left,const BigInteger& right);
+    friend bool operator==(const BigInteger& left,const BigInteger& right);
+    friend bool operator!=(const BigInteger& left,const BigInteger& right);
+    friend bool operator<=(const BigInteger& left,const BigInteger& right);
+    friend bool operator>(const BigInteger& left,const BigInteger& right);
+    friend bool operator>=(const BigInteger& left,const BigInteger& right);
+    friend BigInteger operator+(const BigInteger& left,const BigInteger& right);
+    friend BigInteger operator-(const BigInteger& left,const BigInteger& right);
+
+    BigInteger multiplyByInt(int value) const;
+    int divideByInt(int value);
+private:
+    static const int Base = 1000000000;
+    static const int BaseDigits = 9;
+    std::vector<int> digits_;
+    void removeLeadingZeros();
+};
+
+#endif // BIGINTEGER_H
